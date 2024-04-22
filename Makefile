a48469c440a3dd88377ede628e6f71615764feae
real-splendid <work645@ya.ru>
2024-04-23 00:00:37 +0300

    iter12: добавлен метод shorten batch

---
+++ Makefile
@@ -0,0 +1,24 @@
+TEST_CMD := ./shortenertestbeta -test.v -source-path=. -binary-path=./cmd/shortener/shortener -server-port=8087 -database-dsn='postgres://app:pass@localhost:5432/short?sslmode=disable'
+
+
+test:
+	go test ./internal/...
+
+vet:
+	go vet -vettool=statictest  ./...
+
+test-iteration:
+	go build -o cmd/shortener/shortener cmd/shortener/*.go
+	$(TEST_CMD) -test.run=^TestIteration1$$
+	$(TEST_CMD) -test.run=^TestIteration2$$
+	$(TEST_CMD) -test.run=^TestIteration3$$
+	$(TEST_CMD) -test.run=^TestIteration4$$
+	$(TEST_CMD) -test.run=^TestIteration5$$
+	$(TEST_CMD) -test.run=^TestIteration6$$
+	$(TEST_CMD) -test.run=^TestIteration7$$
+	$(TEST_CMD) -test.run=^TestIteration8$$
+	$(TEST_CMD) -test.run=^TestIteration9$$ -file-storage-path=.vscode/tmp.json
+	$(TEST_CMD) -test.run=^TestIteration10$$ -file-storage-path=.vscode/tmp.json
+	$(TEST_CMD) -test.run=^TestIteration11$$ -file-storage-path=.vscode/tmp.json
+	$(TEST_CMD) -test.run=^TestIteration12$$ -file-storage-path=.vscode/tmp.json
+
---
+++ internal/handlers/api_shorten_batch.go
@@ -0,0 +1,74 @@
+package handlers
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
+
+	"github.com/real-splendid/url-shortener-practicum/internal"
+	"go.uber.org/zap"
+)
+
+type (
+	ShortenBatchReq struct {
+		CorrelationID string `json:"correlation_id"`
+		OriginalURL   string `json:"original_url"`
+	}
+
+	ShortenBatchResp struct {
+		CorrelationID string `json:"correlation_id"`
+		ShortURL      string `json:"short_url"`
+	}
+)
+
+func MakeAPIShortenBatchHandler(storage internal.Storage, logger *zap.SugaredLogger, baseURL string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req []ShortenBatchReq
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			logger.Error(err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		defer r.Body.Close()
+		err = json.Unmarshal(body, &req)
+		if err != nil {
+			logger.Error(err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		var resp []ShortenBatchResp
+		for _, item := range req {
+			if _, err := url.Parse(item.OriginalURL); err != nil {
+				logger.Error(err)
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			key := internal.MakeKey()
+			if err := storage.Set(key, item.OriginalURL); err != nil {
+				logger.Error(err)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			resp = append(resp, ShortenBatchResp{
+				CorrelationID: item.CorrelationID,
+				ShortURL:      baseURL + "/" + key,
+			})
+		}
+
+		jsonResp, err := json.Marshal(resp)
+		if err != nil {
+			logger.Error(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		w.Write(jsonResp)
+	}
+}
